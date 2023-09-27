7a881d85b3d95b4b26f2e8f57114f97eb7711baf
¨Matheus¨ <matheus.sodree@gmail.com>
2023-09-26 23:46:54 -0300

    Firt commit admin

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,11 @@ use App\Http\Controllers\Api\Company\CategoryController;
 use App\Http\Controllers\Api\Company\CompanyController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/', function () {
+    return response()->json(['message' => 'success']);
+});
+
 Route::apiResource('categories',CategoryController::class);
 
 Route::apiResource('companies',CompanyController::class);
